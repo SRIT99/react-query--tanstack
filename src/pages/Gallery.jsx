ba67b73b74604cbecad4f1ea6a3b5c05dd2ff2ba
Suraj <surajworkspace12@gmail.com>
2026-08-04 21:54:06 +0545

    using infinite scroll with react intersection observer package

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,11 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
 import React, { useEffect } from 'react'
 import { fetchPhotos } from '../API/GalleryApi'
+import { useInView } from 'react-intersection-observer'
 
 const Gallery = () => {
 
-    const { data, hasNextPage, fetchNextPage } = useInfiniteQuery({
+    const { data, hasNextPage, fetchNextPage, status, isFetchingNextPage } = useInfiniteQuery({
         queryKey: ["photos"],
         queryFn: fetchPhotos,
         getNextPageParam: (lastPage, allPages) => {
@@ -12,17 +13,27 @@ const Gallery = () => {
             return lastPage.length === 10 ? allPages.length + 1 : undefined
         }
     })
-    const handleScroll = () => {
-        const bottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 1;
-        if (bottom && hasNextPage) {
+    // const handleScroll = () => {
+    //     const bottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 1;
+    //     if (bottom && hasNextPage) {
+    //         fetchNextPage();
+    //     }
+    // }
+    const { ref, inView } = useInView({
+        threshold: 1,
+    })
+    useEffect(() => {
+        if (inView && hasNextPage) {
             fetchNextPage();
         }
+    }, [inView, fetchNextPage, hasNextPage])
+    // useEffect(() => {
+    //     window.addEventListener('scroll', handleScroll);
+    //     return () => window.removeEventListener('scroll', handleScroll);
+    // }, [hasNextPage])
 
-    }
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    })
+    if (status == "loading") return <div>Loading...</div>
+    if (status == "error") return <div>Error Fetching data</div>
     return (
         <div className="gallery-page">
             <h1 className="gallery-title">My Gallery</h1>
@@ -59,6 +70,10 @@ const Gallery = () => {
                     )
                 })
             }
+            <div ref={ref}>
+                {isFetchingNextPage && <div>Loading More..</div>}
+            </div>
+
         </div>
     )
 }
